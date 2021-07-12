75d4da2c7c1dec7343bd39728ee35c81491a9f33
Parkchangun <qkrcksrjs@gmail.com>
2021-07-12 09:54:25 +0900

    backup_Monday

--- front/src/components/users/SignUp.jsx
+++ front/src/components/users/SignUp.jsx
@@ -6,40 +6,108 @@ import {
   Input,
   flexCenterAlign,
   Button,
+  Positive,
+  Negative,
 } from '../../styles/CommonStyle';
 import { useTodoDispatch } from '../context/ToDoContext';
 
-const InsertForm = styled.div`
-  width: 15rem;
-  height: 15rem;
-  background-color: orange;
-`;
-
-const LoginBox = styled.div`
+const SignBox = styled.div`
   ${MainBlock}
   ${flexCenterAlign}
+  flex-direction: column;
+  justify-content: space-around;
 `;
 
+const TitleBox = styled.div`
+  margin-top: 2rem;
+  font-size: 1.5rem;
+`;
+
+const InsertForm = styled.div`
+  ${flexCenterAlign}
+  flex-direction: column;
+  justify-content: space-around;
+  width: 100%;
+  height: 80%;
+`;
+
+const InputWrap = styled.div`
+  ${flexCenterAlign}
+  flex-direction: column;
+  width: 100%;
+`;
 const InputBox = styled.input`
   ${Input}
+  & + & {
+    margin-top: 20px;
+  }
 `;
 
+const ButtonWrap = styled.div`
+  ${flexCenterAlign}
+  flex-direction: column;
+  width: 100%;
+`;
 const ButtonBox = styled.button`
   ${Button}
+  cursor: pointer;
+  ${Positive}
+  & + & {
+    margin-top: 20px;
+    ${Negative}
+  }
 `;
 
 function SignUp() {
+  // const [id, setID] = useState('');
+  // const [pw, setPW] = useState('');
+  const [user, setUser] = useState({
+    id: '',
+    password: '',
+  });
+
+  // const chnageID = (e) => setID(e.target.value);
+  // const changePW = (e) => setPW(e.target.value);
+  const onChange = (e) =>
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  const enterPress = (e) => {
+    if (e.key === 'Enter') onSubmit();
+  };
+  const onSubmit = (e) => {
+    alert(`id = ${user.id} pw = ${user.password}`);
+  };
+
   return (
-    <LoginBox>
+    <SignBox>
+      <TitleBox>회원가입</TitleBox>
       <InsertForm>
-        <InputBox name='id' placeholder='ID'></InputBox>
-        <InputBox type='password' name='pw' placeholder='PW'></InputBox>
-        <ButtonBox>가입하기</ButtonBox>
-        <Link to='/'>
-          <ButtonBox>취소하기</ButtonBox>
-        </Link>
+        <InputWrap>
+          <InputBox
+            name='id'
+            placeholder='ID'
+            onChange={onChange}
+            value={user.id}
+          ></InputBox>
+          <InputBox
+            type='password'
+            name='password'
+            placeholder='PASSWORD'
+            onChange={onChange}
+            value={user.password}
+            onKeyPress={enterPress}
+          ></InputBox>
+        </InputWrap>
+        <ButtonWrap>
+          <ButtonBox onClick={onSubmit}>가입하기</ButtonBox>
+          <ButtonBox>
+            <Link to='/'>취소하기</Link>
+          </ButtonBox>
+        </ButtonWrap>
       </InsertForm>
-    </LoginBox>
+    </SignBox>
   );
 }
 
--- front/src/styles/CommonStyle.js
+++ front/src/styles/CommonStyle.js
@@ -1,8 +1,14 @@
 import { css } from 'styled-components';
 
+export const flexCenterAlign = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 export const MainBlock = css`
   position: absolute;
-  top: 50%;
+  top: 48%;
   left: 50%;
   transform: translate(-50%, -40%);
   background-color: white;
@@ -19,31 +25,36 @@ export const Input = css`
   border-radius: 4px;
   border: 1px solid #dee2e6;
   width: 100%;
-  outline: none;
+  outline-color: rgba(93, 161, 228, 1);
   font-size: 1.3rem;
   box-sizing: border-box;
 `;
 
 export const Button = css`
   width: 100%;
-  padding: 12px;
+  height: 4.5rem;
   font-size: 1.3rem;
   border-radius: 4px;
   border: 1px solid #dee2e6;
+  padding: 0;
   & a {
     color: inherit;
     text-decoration: none;
+    display: block;
+    line-height: 4.4rem;
   }
 `;
 export const Positive = css`
-  background-color: #33ff3f;
+  background-color: rgba(93, 161, 228, 0.7);
+  transition: 1s;
+  &:hover {
+    background-color: rgba(93, 161, 228, 1);
+  }
 `;
 export const Negative = css`
-  background-color: #ff3333;
-`;
-
-export const flexCenterAlign = css`
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  background-color: rgba(125, 228, 93, 0.5);
+  transition: 1s;
+  &:hover {
+    background-color: rgba(125, 228, 93, 1);
+  }
 `;
